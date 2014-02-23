55675ae8d06960dfe0385ba938e238a1459fcc23
Jason Cross <jcross@immense.net>
2014-02-23 04:04:46 -0500

    Sets canonical links

--- config.rb
+++ config.rb
@@ -2,55 +2,8 @@
 # Settings
 ###
 set :site_title, "Jason Cross"
-set :site_url, "http://hellojason.net"
 set :site_description, "Portfolio website of designer Jason Cross."
-require 'bootstrap-sass'
-###
-# Compass
-###
 
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 helpers do
 
@@ -142,9 +95,17 @@ set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
+
+# Development-specific configuration
+configure :development do
+  set :site_url, "http://leonardo.dev:4567"
+end
+
+
 # Build-specific configuration
 configure :build do
   set :sass, style: :compressed
+  set :site_url, "http://hellojason.net"
   activate :minify_css
   #activate :minify_html
   activate :minify_javascript
