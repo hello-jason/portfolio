0acef316ff1b0957a5cd283c10dc73acacb4a456
Jason <jason@hellojason.net>
2015-01-07 00:16:11 -0500

    Imageoptim configuration to optimize all images on every build.

--- config.rb
+++ config.rb
@@ -176,6 +176,8 @@ configure :build do
   # Compress and optimise images during build
   # Documentation: https://github.com/plasticine/middleman-imageoptim
   activate :imageoptim do |options|
+  # Use a build manifest to prevent re-compressing images between builds
+  options.manifest = false
     # Image extensions to attempt to compress
     options.image_extensions = %w(.png .jpg .gif .svg)
     # Cause image_optim to be in shouty-mode
