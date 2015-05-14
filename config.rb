16c7084f128b579e7681c132ceeb04996e3077db
Jason <jason@hellojason.net>
2015-05-14 10:02:13 -0400

    Installed Lightcase, after trying several others. Needs styling.

--- config.rb
+++ config.rb
@@ -142,11 +142,11 @@ helpers do
   end
 
   # Image gallery
-  def image_gallery(image, section, title="")
+  def image_gallery(image, collection, title="")
     # = image_gallery "car.jpg" "cars" "Nice car!"
-    full_image_url = "/#{images_dir}/#{section}/#{image}"
-    thumb_image_url = "/#{images_dir}/#{section}/thumbs/thumbs_#{image}"
-    "<a href='#{full_image_url}'>
+    full_image_url = "/#{images_dir}/#{collection}/#{image}"
+    thumb_image_url = "/#{images_dir}/#{collection}/thumbs/thumbs_#{image}"
+    "<a href='#{full_image_url}' data-rel='lightcase:#{collection}'>
       <img src='#{thumb_image_url}' alt='#{title}' class='img-thumbnail' />
     </a>
     "
--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,8 +1,14 @@
 //= require layzr.js/dist/layzr
+//= require jquery/dist/jquery
+//= require lightcase/lightcase
 
 var layzr = new Layzr({
   attr: 'data-layzr',
   retinaAttr: 'data-layzr-retina',
   threshold: 20,
   callback: null
+});
+
+jQuery(document).ready(function($) {
+  $('a[data-rel^=lightcase]').lightcase();
 });
