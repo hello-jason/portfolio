6aa449ee943983e0f44e9447f5e6e1cda2a17978
Jason Cross <jason@hellojason.net>
2016-06-04 22:41:23 -0500

    Livechat project page and content

--- config.rb
+++ config.rb
@@ -170,8 +170,8 @@ helpers do
 
   # Codepen embed
   # The slug hash determines which codepen to embed
-  def codpen(slugHash="", height="500", defaultTabs="result", user="hellojason")
-    "<div class=\"codepen-container\"><p data-height=\"#{height}\" data-theme-id=\"light\" data-slug-hash=\"#{slugHash}\" data-default-tab=\"#{defaultTabs}\" data-user=\"#{user}\" data-embed-version=\"2\" data-preview=\"true\" class=\"codepen\"></div>"
+  def codpen(slugHash="", height="500", runPen="auto", defaultTabs="result", user="hellojason")
+    "<div class=\"codepen-container\"><p data-height=\"#{height}\" data-theme-id=\"light\" data-slug-hash=\"#{slugHash}\" data-default-tab=\"#{defaultTabs}\" data-user=\"#{user}\" data-embed-version=\"2\""+  (runPen == "click" ? " data-preview=\"true\" " : "") +"class=\"codepen\"></div>"
   end
 
 end
