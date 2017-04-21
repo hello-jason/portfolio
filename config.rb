26fcadd7a9b23ff75f94990cbc51974ccca2484e
Jason Cross <code@hellojason.net>
2017-04-21 18:38:28 -0500

    fix(disqus): configure

--- config.rb
+++ config.rb
@@ -77,75 +77,29 @@ end
 activate :directory_indexes
 
 
-# Build-specific configuration
 # ========================================================================
-configure :build do
-  # Ignore file/dir during build process
-  ignore ".git"
-  ignore "environment_variables.rb"
-  ignore "environment_variables.sample.rb"
-  ignore "favicon_template.png"
-  ignore "article.tt"
-  ignore "imageoptim.manifest.yml"
-
-  set :site_url, "#{site_url_production}"
-
-  # Optimization
-  set :sass, line_comments: false, style: :compressed
-  activate :minify_css
-  activate :minify_html
-  activate :minify_javascript
-  activate :gzip
-
-  # Cache buster
-  activate :asset_hash, :exts => ['.css', '.js']
-
-  # Compress and optimise images during build
-  # Documentation: https://github.com/plasticine/middleman-imageoptim
-  activate :imageoptim do |options|
-    # Use a build manifest to prevent re-compressing images between builds
-    options.manifest = false
-    # Image extensions to attempt to compress
-    options.image_extensions = %w(.jpg .gif .svg)
-    # Cause image_optim to be in shouty-mode
-    options.verbose = false
+# Development-specific configuration
+# ========================================================================
+configure :development do
+  # Disqus comments
+  activate :disqus do |d|
+    # using a special shortname
+    d.shortname = "hello-jason-dev"
+    # or setting to `nil` will stop Disqus loading
+    # d.shortname = nil
   end
+end
+
+
+# ========================================================================
+# Build-specific configuration
+
+configure :build do
+  config[:host]       = 'https://hellojason.net'
 
   # Disqus comments
   activate :disqus do |d|
     # using a different shortname for production builds
     d.shortname = "hellojason"
   end
-
-  # Create favicon and device-specific icons
-  activate :favicon_maker, :icons => {
-    "favicon_template.png" => [
-      { icon: "apple-touch-icon-152x152-precomposed.png" },        # Same as apple-touch-icon-57x57.png, for retina iPad with iOS7.
-      { icon: "apple-touch-icon-144x144-precomposed.png" },        # Same as apple-touch-icon-57x57.png, for retina iPad with iOS6 or prior.
-      { icon: "apple-touch-icon-120x120-precomposed.png" },        # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS7.
-      { icon: "apple-touch-icon-114x114-precomposed.png" },        # Same as apple-touch-icon-57x57.png, for retina iPhone with iOS6 or prior.
-      { icon: "apple-touch-icon-76x76-precomposed.png" },          # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS7.
-      { icon: "apple-touch-icon-72x72-precomposed.png" },          # Same as apple-touch-icon-57x57.png, for non-retina iPad with iOS6 or prior.
-      { icon: "apple-touch-icon-60x60-precomposed.png" },          # Same as apple-touch-icon-57x57.png, for non-retina iPhone with iOS7.
-      { icon: "apple-touch-icon-57x57-precomposed.png" },          # iPhone and iPad users can turn web pages into icons on their home screen. Such link appears as a regular iOS native application. When this happens, the device looks for a specific picture. The 57x57 resolution is convenient for non-retina iPhone with iOS6 or prior. Learn more in Apple docs.
-      { icon: "apple-touch-icon-precomposed.png", size: "57x57" }, # Same as apple-touch-icon.png, expect that is already have rounded corners (but neither drop shadow nor gloss effect).
-      { icon: "apple-touch-icon.png", size: "57x57" },             # Same as apple-touch-icon-57x57.png, for "default" requests, as some devices may look for this specific file. This picture may save some 404 errors in your HTTP logs. See Apple docs
-      { icon: "favicon-196x196.png" },                             # For Android Chrome M31+.
-      { icon: "favicon-160x160.png" },                             # For Opera Speed Dial (up to Opera 12; this icon is deprecated starting from Opera 15), although the optimal icon is not square but rather 256x160. If Opera is a major platform for you, you should create this icon yourself.
-      { icon: "favicon-96x96.png" },                               # For Google TV.
-      { icon: "favicon-32x32.png" },                               # For Safari on Mac OS.
-      { icon: "favicon-16x16.png" },                               # The classic favicon, displayed in the tabs.
-      { icon: "favicon.png", size: "16x16" },                      # The classic favicon, displayed in the tabs.
-      { icon: "favicon.ico", size: "64x64,32x32,24x24,16x16" },    # Used by IE, and also by some other browsers if we are not careful.
-      { icon: "mstile-70x70.png", size: "70x70" },                 # For Windows 8 / IE11.
-      { icon: "mstile-144x144.png", size: "144x144" },
-      { icon: "mstile-150x150.png", size: "150x150" },
-      { icon: "mstile-310x310.png", size: "310x310" },
-      { icon: "mstile-310x150.png", size: "310x150" }
-    ]
-  }
-end
-
-configure :build do
-  config[:site_url]       = 'https://hellojason.net'
 end
